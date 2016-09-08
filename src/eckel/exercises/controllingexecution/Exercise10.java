9e4afafa07a227acc84e4253b2beaedd651b7730
Andriana <andrianay@gmail.com>
2016-09-09 01:49:49 +0300

    week3 branch

--- src/eckel/exercises/controllingexecution/Exercise10.java
+++ src/eckel/exercises/controllingexecution/Exercise10.java
@@ -10,69 +10,95 @@ package eckel.exercises.controllingexecution;
  Write a program that finds all the 4-digit vampire numbers. (Suggested by Dan Forhan.)
  */
 
-
-import static net.mindview.util.Print.print;
-
 /**
  * Created by Andriana on 01.09.2016.
  */
 public class Exercise10 {
     public static void main(String[] args) {
+
         int[] vN = new int[4];
-        int firstHorn;
-        int secHorn;
-        Integer[] vampN;
-        Integer[] notVampN;
-        int totalTest = 0;
-        print("something");
-
-
 
         for (int i = 1000; i <= 9999; i++){
-            firstHorn = i / 100;
-            System.out.print(firstHorn + " * ");
-            secHorn = i % 100;
-            System.out.println(secHorn);
+            vN[0] = i / 100 / 10;
+            vN[1] = i / 100 % 10;
+            vN[2] = i % 100 / 10;
+            vN[3] = i % 100 % 10;
 
-
-//            if((firstHorn * secHorn) == i ){
-//                System.out.println(i + " is a vampire number");
-
-                vN[0] = firstHorn / 10;
-                vN[1] = firstHorn % 10;
-                vN[2] = secHorn / 10;
-                vN[3] = secHorn % 10;
-
-                for(int k = 0; k < 4; k++){
-                    for(int l = 0; l < 4; l ++){
-                        if(l > 3)
-                             l=0;
-                        if(l == k)
-                            l++;
-                        for (int m = 0; m < 4; m++){
-                            if(m > 3)
-                                l=0;
-                            if (m == k)
-                                m++;
-                            if (m == l)
-                                m++;
-                            for(int n = 0; n < 4; n++){
-                                if(n > 3)
-                                    l=0;
-                                if(n == k)
-                                    n++;
-                                if(n == l)
-                                    n++;
-                                if (n == m)
-                                    n++;
-                                if(((vN[k] * 10 + vN[l]) * (vN[m] * 10 + vN[n]))== i)
-                                    System.out.println(totalTest + ": " + vN[k] + vN[l] + vN[m] + vN[n]);
-                            }
-                        }
-                    }
+            if(((vN[0] * 10 + vN[1]) != 0) && ((vN[2] * 10 + vN[3])!= 0))
+                if(((vN[0] * 10 + vN[1]) * (vN[2] * 10 + vN[3]))== i) {
+                    System.out.println(i + " is vampire : " + vN[0] + vN[1] + "*" + vN[2] + vN[3]);
+                    continue;
                 }
-//            }
 
+            if(((vN[1] * 10 + vN[0]) != 0) && ((vN[2] * 10 + vN[3])!= 0))
+                if(((vN[1] * 10 + vN[0]) * (vN[2] * 10 + vN[3]))== i) {
+                    System.out.println(i + " is vampire : " + vN[1] + vN[0] + "*" + vN[2] + vN[3]);
+                    continue;
+                }
+
+            if(((vN[0] * 10 + vN[1]) != 0) && ((vN[3] * 10 + vN[2])!= 0))
+                if(((vN[0] * 10 + vN[1]) * (vN[3] * 10 + vN[2]))== i) {
+                    System.out.println(i + " is vampire : " + vN[0] + vN[1] + "*" + vN[3] + vN[2]);
+                    continue;
+                }
+
+            if(((vN[1] * 10 + vN[0]) != 0) && ((vN[3] * 10 + vN[2])!= 0))
+                if(((vN[1] * 10 + vN[0]) * (vN[3] * 10 + vN[2]))== i) {
+                    System.out.println(i + " is vampire : " + vN[1] + vN[0] + "*" + vN[3] + vN[2]);
+                    continue;
+                }
+
+
+
+            if(((vN[0] * 10 + vN[2]) != 0) && ((vN[1] * 10 + vN[3])!= 0))
+                if(((vN[0] * 10 + vN[2]) * (vN[1] * 10 + vN[3]))== i) {
+                    System.out.println(i + " is vampire : " + vN[0] + vN[2] + "*" + vN[1] + vN[3]);
+                    continue;
+                }
+
+            if(((vN[2] * 10 + vN[0]) != 0) && ((vN[1] * 10 + vN[3])!= 0))
+                if(((vN[2] * 10 + vN[0]) * (vN[1] * 10 + vN[3]))== i) {
+                    System.out.println(i + " is vampire : " + vN[2] + vN[0] + "*" + vN[1] + vN[3]);
+                    continue;
+                }
+
+            if(((vN[0] * 10 + vN[2]) != 0) && ((vN[3] * 10 + vN[1])!= 0))
+                if(((vN[0] * 10 + vN[2]) * (vN[3] * 10 + vN[1]))== i) {
+                    System.out.println(i + " is vampire : " + vN[0] + vN[2] + "*" + vN[3] + vN[1]);
+                    continue;
+                }
+
+            if(((vN[2] * 10 + vN[0]) != 0) && ((vN[3] * 10 + vN[1])!= 0))
+                if(((vN[2] * 10 + vN[0]) * (vN[3] * 10 + vN[1]))== i) {
+                    System.out.println(i + " is vampire : " + vN[2] + vN[0] + "*" + vN[3] + vN[1]);
+                    continue;
+                }
+
+
+
+            if(((vN[0] * 10 + vN[3]) != 0) && ((vN[2] * 10 + vN[1])!= 0))
+                if(((vN[0] * 10 + vN[3]) * (vN[2] * 10 + vN[1]))== i) {
+                    System.out.println(i + " is vampire : " + vN[0] + vN[3] + "*" + vN[2] + vN[1]);
+                    continue;
+                }
+
+            if(((vN[3] * 10 + vN[0]) != 0) && ((vN[2] * 10 + vN[1])!= 0))
+                if(((vN[3] * 10 + vN[0]) * (vN[2] * 10 + vN[1]))== i) {
+                    System.out.println(i + " is vampire : " + vN[3] + vN[0] + "*" + vN[2] + vN[1]);
+                    continue;
+                }
+
+            if(((vN[0] * 10 + vN[3]) != 0) && ((vN[1] * 10 + vN[2])!= 0))
+                if(((vN[0] * 10 + vN[3]) * (vN[1] * 10 + vN[2]))== i) {
+                    System.out.println(i + " is vampire : " + vN[0] + vN[3] + "*" + vN[1] + vN[2]);
+                    continue;
+                }
+
+            if(((vN[3] * 10 + vN[0]) != 0) && ((vN[1] * 10 + vN[2])!= 0))
+                if(((vN[3] * 10 + vN[0]) * (vN[1] * 10 + vN[2]))== i) {
+                    System.out.println(i + " is vampire : " + vN[3] + vN[0] + "*" + vN[1] + vN[2]);
+                    continue;
+                }
         }
     }
 }
